586b5d4967ae059494dc89fa9acd0a401ba31872
honakac <honakac@yandex.ru>
2026-03-28 20:23:39 +0500

    update help message

--- client/commands/help.go
+++ client/commands/help.go
@@ -3,6 +3,8 @@ package commands
 import "fmt"
 
 func Help() {
-	fmt.Println("generate          Generate new keys")
-	fmt.Println("exit              Just exit")
+	fmt.Println("generate                              Generate new keys")
+	fmt.Println("encrypt recipientID message           Encrypt message")
+	fmt.Println("decrypt senderID message              Decrypt message")
+	fmt.Println("exit                                  Just exit")
 }
